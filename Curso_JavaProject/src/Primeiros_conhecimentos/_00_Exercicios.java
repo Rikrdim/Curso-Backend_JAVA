1e5bfca532ac6a38a72bd69fba2382812f51199f
Ricardo Filho <rikrdopfh@outlook.com>
2022-05-26 01:36:09 -0300

    Atualização de arquivos1

---
+++ Curso_JavaProject/src/Primeiros_conhecimentos/_00_Exercicios.java
@@ -0,0 +1,25 @@
+package Primeiros_conhecimentos;
+
+public class _00_Exercicios {
+	
+	public static void main(String[] args) {
+	
+		//================= Exercícios =========================
+		
+		// Calcule á área do triangulo, sendo b3=6, B3=8 e h=5
+		
+				double b3, B3, h, area;
+					
+				b3 = 6.0;
+				B3 = 8.0;
+				h = 5.0;
+				
+				area = (b3 +B3) / 2 * h;
+				
+				System.out.println(area);
+			
+				//É uma boa prática utilizar o [ .0 ] quando é utilizado o double. Assim como também é uma boa prática utilizar o [ f ]
+				//quando se trata de um float
+	}
+
+}
