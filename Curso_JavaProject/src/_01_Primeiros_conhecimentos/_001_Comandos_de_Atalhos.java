7965007eda7fb528cae7292f9320e6c5aa48327f
Ricardo Filho <rikrdopfh@outlook.com>
2022-06-06 06:21:48 -0300

    Ajuste no arquivo de exercício e adição das estruturas condicionais parte 1

---
+++ Curso_JavaProject/src/_01_Primeiros_conhecimentos/_001_Comandos_de_Atalhos.java
@@ -0,0 +1,17 @@
+package _01_Primeiros_conhecimentos;
+
+public class _001_Comandos_de_Atalhos {
+
+	public static void main(String[] args) {
+		
+		
+		// Sysout + Ctrl + barra de espaço = System.out.println();
+		
+		// Ctrl + shift + f = identa o código de forma automática
+		
+		//
+		
+
+	}
+
+}
---
+++ Curso_JavaProject/src/_02_Estruturas_Condicionais/_02_Expressoes_Logicas.java
@@ -0,0 +1,49 @@
+package _02_Estruturas_Condicionais;
+
+public class _02_Expressoes_Logicas {
+
+	public static void main(String[] args) {
+		// Expressões Lógicas
+		//                                                              |                          |
+		                                       // Tabela operador E   | |    Tabela operador OU    |    Tabela operador NÃO
+		//                                                              |                          |
+		// OPERADOR        SIGNIFICADO         |__    A   B   A&&B  __| | |__    A   B   A||B  __| | |__    A  !A  __|
+		//                                      _____             _____ |  _____             _____ |  _____       ___
+		//    &&               E               |      F   F     F     | | |      F   F     F     | | |      F   V    |
+		//	  ||               OU              |      F   V     F     | | |      F   V     V     | | |      V   F    |
+		//	  !                NÃO             |      V   F     F     | | |      V   F     V     | | 
+        //                                     |      V   V     V     | | |      V   V     V     | |
+        //                                     |                        |                          |
+//*****************************************************************************************************************************
+		
+		//EXEMPLO [ x_ele|ou|nao = variavel x + expressão lógica ]
+		
+// Supondo que x_ele = 5 Responda: x_ele <= 20 && x_ele == 10 | x_ele > 0 && x_ele != 3 | x_ele <=20 && x_ele == 10 && x_ele != 3
+		
+		int x_ele = 5;
+		
+		System.out.println(x_ele <= 20 && x_ele == 10);
+		System.out.println(x_ele > 0 && x_ele != 3);
+		System.out.println(x_ele <=20 && x_ele == 10 && x_ele != 3);
+		System.out.println();
+		
+	
+	// Responda x_elou == 10 || x_elou <= 20 | x_elou > 0 || x_elou != 3 | x_elou <= 0 || x_elou != 3 || x_elou != 5 
+		
+		int x_elou = 5;
+		
+		System.out.println(x_elou == 10 || x_elou <= 20);
+		System.out.println(x_elou > 0 || x_elou != 3);
+		System.out.println(x_elou <= 0 || x_elou != 3 || x_elou != 5);
+		System.out.println();
+		
+	// Responda !(x_elnao == 10) | !(x_elnao <= 2) | !(x_elnao <= 20 && x_elnao == 10)
+		
+		int x_elnao = 5;
+		
+		System.out.println(!(x_elnao == 10));
+		System.out.println(!(x_elnao <= 2));
+		System.out.println(!(x_elnao <= 20 && x_elnao == 10));
+	}
+
+}
